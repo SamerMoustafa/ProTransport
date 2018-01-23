57bd295da61e702cbec10d16fe044fae4a0884d1
SamDev <sam.zcodr@hotmail.com>
2018-01-24 00:25:39 +0200

    Init

---
+++ ProTransport/Connection/Database.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProTransport.Connection
+{
+    class Database
+    {
+        static String Host { get { return "127.0.0.1"; } }
+        static String Name { get { return "Transport"; } }
+        static String Username { get { return "root"; } }
+        static String Password { get { return ""; } }
+
+        public static String ConnectionString
+        {
+            get { return String.Format("Server={0};Uid={1};Pwd={2};Database={3};", Host, Username, Password, Name); }
+        }
+
+        public static MySqlConnection Connection { get; protected set; }
+
+        public static void Initialize()
+        {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+        }
+
+    }
+}
---
+++ ProTransport/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProTransport.Views;
+using ProTransport.Connection;
+
+namespace ProTransport
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Database.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
