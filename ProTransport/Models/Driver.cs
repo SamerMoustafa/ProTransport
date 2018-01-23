6690f7bb445c2d34093e049142fd99ba2d6dc7b9
SamDev <sam.zcodr@hotmail.com>
2018-01-24 01:22:01 +0200

    Add Dapper Extensions References

---
+++ ProTransport/Models/Driver.cs
@@ -0,0 +1,22 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProTransport.Models
+{
+    class Driver
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Salary { get; set; }
+        public String Phone { get; set; }
+        public String Address { get; set; }
+        public int Locked { get; set; }
+        public int FavouriteCar { get; set; }
+
+    }
+}
