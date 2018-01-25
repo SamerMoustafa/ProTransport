0f012bd38d0b01f1390c24a55fd335e97d79d566
sam <sam.elkady@hotmail.com>
2018-01-25 11:56:42 +0200

    ClientClass

---
+++ ProTransport/Models/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace ProTransport.Models
+{
+    class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CompanyPhone { get; set; }
+        public string CompanyMail { get; set; }
+        public string AccountantPhone { get; set; }
+        public string AccountantMail { get; set; }
+    }
+}
