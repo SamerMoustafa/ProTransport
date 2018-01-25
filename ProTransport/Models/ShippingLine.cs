c5f76a081b81ab25521bb8dbc071581c4c38ad80
SamDev <sam.zcodr@hotmail.com>
2018-01-25 23:26:06 +0200

    Models

---
+++ ProTransport/Models/ShippingLine.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProTransport.Models
+{
+    [Table("shipping_lines")]
+    class ShippingLine
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String Phone { get; set; }
+        public String Mail { get; set; }
+    }
+}
---
+++ ProTransport/Models/Tour.cs
@@ -0,0 +1,23 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProTransport.Models
+{
+    class Tour
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int DriverId { get; set; }
+        public int CarId { get; set; }
+        public int ContainersCount { get; set; }
+        public String ContainersType { get; set; }
+        public int ShippingLine { get; set; }
+        public String BillNo { get; set; }
+        public String BookingNo { get; set; }
+    }
+}
