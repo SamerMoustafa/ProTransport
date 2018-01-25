5a04ceb93d82e977a5afaf7211ddabc43398b1b1
sam <sam.elkady@hotmail.com>
2018-01-26 00:05:40 +0200

    User Class

---
+++ ProTransport/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace ProTransport.Models
+{
+    class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Usename { get; set; }
+        public string Paasword { get; set; }
+        public string Role { get; set; }
+    }
+}
